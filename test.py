bd54a1dc4a1168238a6799f97b687c9dd44d9534
Tisone Kironget <tisonkironget@gmail.com>
2026-02-19 17:48:25 +0300

    chore: cleanup root directory - remove one-off scripts and organize utility files

--- test.py
+++
@@ -1,2 +0,0 @@
-from src.selectors.strategies.base import StrategyFactory; strategies = [{'type': 'text_anchor', 'anchor_text': 'test'}, {'type': 'css', 'selector': '.test'}, {'type': 'xpath', 'selector': '//div'}]; [print(f'✅ {c[\
-type\]}: {StrategyFactory.create_strategy(c).type}') for c in strategies]; print('All strategies work!')
